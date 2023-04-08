c5a8282b62e99252a97fd9df5173db473b3498ba
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-04-08 22:57:00 +0700

    .

---
+++ Jocasta/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Jocasta.Models;
+using Jocasta.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jocasta.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            // danh sách loại phòng mới
+            RoomCategoryService roomCategoryService = new RoomCategoryService();
+            List<RoomCategory> lsRoomCategoryNew = roomCategoryService.GetListRoomCategoryForHomePage();
+            ViewBag.ListRoomCategoryNew = lsRoomCategoryNew;
+
+            // danh sách dịch vụ mới
+            ServiceRoomService serviceRoomService = new ServiceRoomService();
+            List<Service> lsServiceNew = serviceRoomService.GetListServiceForHomePage();
+            ViewBag.ListServiceNew = lsServiceNew;
+
+            return View();
+        }
+
+        [Route("dang-nhap")]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [Route("dang-ky")]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Jocasta/Controllers/RoomCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Jocasta.Models;
+using Jocasta.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jocasta.Controllers
+{
+    public class RoomCategoryController : Controller
+    {
+        // GET: RoomCategory
+        [Route("room-category/danh-sach/{page?}/{keyword?}")]
+        public ActionResult Index(RoomCategorySearchModel searchModel)
+        {
+            RoomCategorySearchModel roomCategorySearch = new RoomCategorySearchModel();
+            roomCategorySearch.CurrentPage = searchModel.CurrentPage;
+            roomCategorySearch.CurrentPage =1;
+            roomCategorySearch.Keyword = searchModel.Keyword;
+            
+            RoomCategoryService roomCategoryService = new RoomCategoryService();
+            ListRoomCategoryView roomCategory = roomCategoryService.GetListRoomCategory(roomCategorySearch);
+            ViewBag.ListRoomCategory = roomCategory;
+            return View();
+        }
+    }
+}
