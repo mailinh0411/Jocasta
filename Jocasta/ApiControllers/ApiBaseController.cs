91c10085640846cdd0b4bec14590a7b907fefdc5
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-04-08 22:11:45 +0700

    .

---
+++ Jocasta/ApiControllers/ApiBaseController.cs
@@ -0,0 +1,28 @@
+﻿using Jocasta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Jocasta.ApiControllers
+{
+    public class ApiBaseController : ApiController
+    {
+        public JsonResult Success(object data = null, string message = null)
+        {
+            return new JsonResult { status = JsonResult.Status.SUCCESS, data = data, message = message };
+        }
+
+        public JsonResult Error(string message = JsonResult.Message.ERROR_SYSTEM)
+        {
+            return new JsonResult { status = JsonResult.Status.ERROR, message = message };
+        }
+
+        public JsonResult Unauthorized()
+        {
+            return new JsonResult { status = JsonResult.Status.UNAUTHORIZED, message = JsonResult.Message.TOKEN_EXPIRED };
+        }
+
+    }
+}
---
+++ Jocasta/ApiControllers/RoomCategoryController.cs
@@ -0,0 +1,27 @@
+﻿using Jocasta.Models;
+using Jocasta.Providers;
+using Jocasta.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Jocasta.ApiControllers
+{
+    public class RoomCategoryController : ApiBaseController
+    {
+        // GET: RoomCategory
+        [HttpGet]
+        public JsonResult GetListRoomCategory()
+        {
+            try
+            {
+                return Success();
+            }catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+    }
+}
