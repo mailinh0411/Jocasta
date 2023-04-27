e3847915c84850ab8e18afaff3ae99226e610e68
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-04-27 23:51:09 +0700

    .

--- Jocasta/Models/CartDetail.cs
+++ Jocasta/Models/CartDetail.cs
@@ -20,7 +20,7 @@ namespace Jocasta.Models
         public string RoomCategoryId { get; set; }
         public string Name { get; set; }
         public string Image { get; set; }
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public int Quantity { get; set; }
     }
 
