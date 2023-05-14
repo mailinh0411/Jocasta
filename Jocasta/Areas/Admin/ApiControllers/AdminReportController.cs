42da174ee4a7d386846272bbc6c28f2df0886544
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-05-14 14:43:42 +0700

    .

---
+++ Jocasta/Areas/Admin/ApiControllers/AdminReportController.cs
@@ -0,0 +1,40 @@
+﻿using Jocasta.Areas.Admin.Services;
+using Jocasta.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace Jocasta.Areas.Admin.ApiControllers
+{
+    public class AdminReportController : ApiBaseAdminController
+    {
+        [HttpGet]
+        public JsonResult GetAllReportDaiy(long from, long to)
+        {
+            try
+            {
+                AdminReportService adminReportService = new AdminReportService();
+                return Success(adminReportService.GetAllReportDaily(from, to));
+            }catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GetAllReportMonth(long from, long to)
+        {
+            try
+            {
+                AdminReportService adminReportService = new AdminReportService();
+                return Success(adminReportService.GetAllReportMonth(from, to));
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+    }
+}
