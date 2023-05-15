70f0fe8ceeba4fa67877238f06d52c6ed622a887
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-05-16 00:42:31 +0700

    order-detail

---
+++ Jocasta/Areas/Admin/ApiControllers/AdminInvoiceController.cs
@@ -0,0 +1,61 @@
+﻿using Jocasta.Models;
+using Jocasta.Providers;
+using Jocasta.Services;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Razor.Tokenizer.Symbols;
+using System.Web.UI.WebControls;
+using System.Web.UI;
+using Jocasta.Areas.Admin.Services;
+
+
+namespace Jocasta.Areas.Admin.ApiControllers
+{
+    public class AdminInvoiceController : ApiBaseAdminController
+    {
+        [HttpGet]
+        public JsonResult GetListRoomBooked(string orderId)
+        {
+            try
+            {
+                AdminInvoiceService adminInvoiceService = new AdminInvoiceService();
+                Invoice invoice = adminInvoiceService.GetInvoiceBooking(orderId);
+                if (invoice == null) throw new Exception("Không có hóa đơn đặt phòng của đơn này.");
+
+                return Success(adminInvoiceService.GetListBookingService(invoice.InvoiceId));
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public JsonResult GetListServiceBooked(string orderId)
+        {
+            try
+            {
+                AdminInvoiceService adminInvoiceService = new AdminInvoiceService();
+                List<Invoice> listInvoice = adminInvoiceService.GetInvoiceService(orderId);
+
+
+                List<object> listDetail = new List<object>();
+
+                foreach (Invoice invoice in listInvoice)
+                {
+                    listDetail.AddRange(adminInvoiceService.GetListServiceBooked(invoice.InvoiceId));
+                }
+
+                return Success(listDetail);
+            }
+            catch (Exception ex)
+            {
+                return Error(ex.Message);
+            }
+        }
+    }
+}
