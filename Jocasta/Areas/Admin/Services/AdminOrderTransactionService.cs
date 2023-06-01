485972bbcb99f82e10e0eee1383f9d67a576b73b
Lynhh.11 <72445965+mailinh0411@users.noreply.github.com>
2023-06-01 12:05:59 +0700

    .

---
+++ Jocasta/Areas/Admin/Services/AdminOrderTransactionService.cs
@@ -0,0 +1,30 @@
+﻿using Dapper;
+using Jocasta.Models;
+using Jocasta.Services;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Jocasta.Areas.Admin.Services
+{
+    public class AdminOrderTransactionService : BaseService
+    {
+        public AdminOrderTransactionService() : base() { }
+        public AdminOrderTransactionService(IDbConnection db) : base(db) { }
+
+        public List<OrderTransaction> GetListOrderTransactionByOrderId(string orderId, IDbTransaction transaction = null)
+        {
+            string query = "select * from [order_transaction] where OrderId = @orderId";
+            return this._connection.Query<OrderTransaction>(query, new { orderId }, transaction).ToList();
+        }
+
+        public void InsertOrderTransaction(OrderTransaction model, IDbTransaction transaction = null)
+        {
+            string query = "INSERT INTO [dbo].[order_transaction] ([OrderTransactionId],[OrderId],[UserAdminId],[Status],[Content],[CreateTime]) VALUES (@OrderTransactionId,@OrderId,@UserAdminId,@Status,@Content,@CreateTime)";
+            int count = this._connection.Execute(query, model, transaction);
+            if (count <= 0) throw new Exception(JsonResult.Message.ERROR_SYSTEM);
+        }
+    }
+}
